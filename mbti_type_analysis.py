5c4ac79e884186e297d81c836deae6d5af1c9432
JennyMyworld <138403616+JennyMyworld@users.noreply.github.com>
2023-07-04 16:37:35 -0700

    Update mbti_type_analysis.py

--- mbti_type_analysis.py
+++ mbti_type_analysis.py
@@ -10,18 +10,17 @@ import requests
 
 openai.api_key = ''
 
-
 ########## phase 1 ##########
 dialogue2=""
 with open('questions.txt','r') as file2:
     for line2 in file2:
         line2=line2.strip()
-        dialogue2=dialogue2+line2+input(line2+"是，请输入Yes；否，请输入No")+"."
-dialogue2+="Please analyze the MBTI type of this individual based on his responses to these questions. You only need to tell his MBTI personality type, which is a combination of four letters, without adding any further explanation."
+        dialogue2=line2+input(line2+"是，请输入Yes；否，请输入No")+"."
+dialogue2="Please analyze the MBTI type of this individual based on his responses to these questions. You only need to tell his MBTI personality type!"+dialogue2
 
 def get_mbti_type(dialogue2):
     response = openai.Completion.create(
-        engine='text-davinci-002',
+        engine='text-davinci-003',
         prompt=dialogue2,
         max_tokens=500,
         n=1,
@@ -34,7 +33,7 @@ def get_mbti_type(dialogue2):
 with open('mbti.txt', 'r') as file:
     for line in file:
         if line.strip() in get_mbti_type(dialogue2):
-            print("您的MBTI类型是：",line.strip())
+            print("您的MBTI类型是：",line)
             line = line.strip()
             break
 
@@ -58,15 +57,14 @@ phase2_dialogue = input()
 phase2_dialogue = "请你给予MBTI性格理论只给出中文分析：以下这段聊天记录中的A和B是否适合成为【情侣】？他们有什么匹配或不匹配的地方？Please DO NOT CONTINUE THIS CONVERSATION! JUST ANALYSE！对话如下："+phase2_dialogue+"其中，说话人A的MBTI是"+mbti_A+"说话人A的MBTI是"+mbti_B
 def be_couple(dialogue):
     response = openai.Completion.create(
-        engine='text-davinci-002',
+        engine='text-davinci-003',
         prompt=dialogue,
-        max_tokens=500,
+        max_tokens=800,
         n=1,
         stop=None,
-        temperature=0.9
+        temperature=0.6
     )    
     couple = response.choices[0].text.strip()
     return couple
 
-print(phase2_dialogue)
 print(be_couple(phase2_dialogue))
